ba7e9d196e378658f4f56bbef6286bbb5a8a864e
youngjinkim <youngjin1293@naver.com>
2023-07-21 14:41:13 +0900

    Path Variable(useParams)
    Query String(useSearchParams)
    
    Page Moving(useNavigate)

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,42 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id:", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode:", mode);
+
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 수정페이지 입니다.</p>
+      <button
+        onClick={() => {
+          setSearchParams({ who: "kimyoungjin" });
+        }}
+      >
+        changQS
+      </button>
+
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        HOME으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        back
+      </button>
     </div>
   );
 };
